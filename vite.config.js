0b73bf693c616b14e4eefca78216cae6a1450708
klahssj <sawyerklahs@gmail.com>
2026-01-23 09:37:17 -0600

    Configurations

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ui/program-catalog/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      'src': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   test: {
     environment: 'jsdom',
     globals: true
